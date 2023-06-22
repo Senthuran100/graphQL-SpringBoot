fb5a621c41b4cc6ce0242bbe97fbbad507247f8b
Senthuran100 <senthuran.manoharan@gmail.com>
2023-06-22 16:00:00 +1000

    Add the cors issue changes.

--- src/main/java/com/senthuran/grpahQL/TryOutGrpahQlApplication.java
+++ src/main/java/com/senthuran/grpahQL/TryOutGrpahQlApplication.java
@@ -3,6 +3,9 @@ package com.senthuran.grpahQL;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEncryptableProperties
@@ -12,4 +15,13 @@ public class TryOutGrpahQlApplication {
         SpringApplication.run(TryOutGrpahQlApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/graphql").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
